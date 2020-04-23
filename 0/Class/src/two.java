36f1ccaf0da5b4c854cacacc08c78e0c5ecab59c
Ali Kaghazgaran <59315074+Alikaghazgaran@users.noreply.github.com>
2020-04-23 21:04:09 +0430

    Add files via upload

---
+++ 0/Class/src/two.java
@@ -0,0 +1,10 @@
+public class two {
+    public static void main(String[] args) {
+        int max=(-1000) , arr[]={0,2,4,6,8,7,9,3,5,1};
+        for (int x : arr) {
+            if (x > max)
+                max = x;
+        }
+        System.out.println("Maximum: " + max);
+    }
+}
